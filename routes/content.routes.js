f2624929cede372edd651fce65d292d6e58179c0
Alexandre Lima <alexandreatlima@gmail.com>
2021-07-27 15:34:33 -0300

    content routes to get by id ok get by name undone

--- routes/content.routes.js
+++ routes/content.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const ContentModel = require("../models/Content.model");
 
-// Puxar infos do filme pelo nome (R)
+// Puxar infos do filme por ID (R)
 
 router.get(
   "/:contentType/:contentId/contentInfos",
@@ -16,6 +16,26 @@ router.get(
       const contentInfos = await ContentModel.findOne({
         contentType: contentType,
         contentId: contentId,
+      });
+      return res.status(200).json(contentInfos);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+// Puxar infos do filme por nome (R) - Rota de search
+
+router.get(
+  "/:searchParams/contentInfos",
+  isAuthenticated,
+  attachCurrentUser,
+
+  async (req, res, next) => {
+    try {
+      const { searchParams } = req.params;
+      const contentInfos = await ContentModel.findOne({
+        original_title: searchParams,
       });
       return res.status(200).json(contentInfos);
     } catch (err) {
