c70fe62a19936f50e20217137c1af0a80d02c7e3
Alexandre Lima <alexandreatlima@gmail.com>
2021-07-27 15:33:42 -0300

    model ajust

--- models/Content.model.js
+++ models/Content.model.js
@@ -4,10 +4,10 @@ const mongoose = require("mongoose");
 const ContentSchema = new Schema({
   contentType: { type: String, required: true, enum: ["movie", "serie"] },
   contentId: { type: Number, required: true },
-  original_name: { type: String, required: true },
+  original_title: { type: String, required: true },
   popularity: { type: Number, required: true },
-  like: { type: Number, required: true, default: 0 },
-  dislike: { type: Number, required: true, default: 0 },
+  likeCounter: { type: Number, required: true, default: 0 },
+  dislikeCounter: { type: Number, required: true, default: 0 },
 });
 
 const ContentModel = model("Content", ContentSchema);
