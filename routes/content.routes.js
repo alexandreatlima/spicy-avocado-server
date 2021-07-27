bb5f5769c1c44b7f9c911cbbd76153e7495b2876
Alexandre Lima <alexandreatlima@gmail.com>
2021-07-27 15:27:29 -0300

    get content by the name done

--- routes/content.routes.js
+++ routes/content.routes.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-const CommentsModel = require("../models/Comments.model");
+const ContentModel = require("../models/Content.model");
 
-// Puxar infos do filme pelo nome
+// Puxar infos do filme pelo nome (R)
+
+router.get(
+  "/:contentType/:contentId/contentInfos",
+  isAuthenticated,
+  attachCurrentUser,
+
+  async (req, res, next) => {
+    try {
+      const { contentType, contentId } = req.params;
+      const contentInfos = await ContentModel.findOne({
+        contentType: contentType,
+        contentId: contentId,
+      });
+      return res.status(200).json(contentInfos);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 module.exports = router;
