a9c0e0cf07fa73699700c808e7ddae1acd7b8041
Alexandre Lima <alexandreatlima@gmail.com>
2021-07-27 15:19:47 -0300

    start to create content routes

---
+++ routes/content.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const CommentsModel = require("../models/Comments.model");
+
+// Puxar infos do filme pelo nome
+
+module.exports = router;
